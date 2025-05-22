0a950b61958aa401a0764998269e5ca029937d48
Nithin Vikas <nithinvikas8389@gmail.com>
2025-05-22 12:31:44 +0530

    feat: Added RAG retreival only programs using threshold.

---
+++ 4_rag/1b_rag_basics.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+load_dotenv()
+api_key = os.getenv("GOOGLE_API_KEY")
+
+# Define the persistent directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+persistent_directory = os.path.join(current_dir, "db", "chroma_db")
+
+# Define the embedding model
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
+
+# Load the existing vector store with the embedding function
+db = Chroma(persist_directory=persistent_directory,
+            embedding_function=embeddings)
+
+# Define the user's question
+query = "Who is Penelope?"
+
+# Retrieve relevant documents based on the query
+retriever = db.as_retriever(
+    search_type="similarity_score_threshold",
+    search_kwargs={"k": 3, "score_threshold": 0.3},
+)
+relevant_docs = retriever.invoke(query)
+
+# Display the relevant results with metadata
+print("\n--- Relevant Documents ---")
+for i, doc in enumerate(relevant_docs, 1):
+    print(f"Document {i}:\n{doc.page_content}\n")
+    if doc.metadata:
+        print(f"Source: {doc.metadata.get('source', 'Unknown')}\n")
